abe9dc1d9bc2b15b5fd92aa908465e5ea41e2a0b
Dignan-Jas <dgjaslowich@gmail.com>
2026-07-02 14:52:17 -0400

    Fixed wall rotation bug

--- client/src/components/WallEntity.tsx
+++ client/src/components/WallEntity.tsx
@@ -4,21 +4,22 @@ import { ThickEdges } from './OutlineMaterial';
 
 type WallEntityProps = {
     position?: [number, number, number];
+    gridx: number;
 };
 
 const wallColor = "#ff0000";
 const verticalRotation = [0, 0, 0];
 const horizontalRotation = [0, -Math.PI / 2, 0];
 
-export function WallEntity({ position = [0, 0, 0] }: WallEntityProps) {
+export function WallEntity({ position = [0, 0, 0], gridx }: WallEntityProps) {
     const cubeRef = useRef<THREE.Mesh>(null);
 
     let currRotation = verticalRotation;
-    if ((position[0] % 1) == 0) currRotation = horizontalRotation;
+    if ((gridx % 1) == 0) currRotation = horizontalRotation;
 
     return(
         <mesh ref={cubeRef} position={position} rotation={currRotation}>
-            <boxGeometry args={[0.25,1,2]} />
+            <boxGeometry args={[0.1,1,2.6]} />
             <meshStandardMaterial color={wallColor} />
         </mesh>
     );
